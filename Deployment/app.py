22c433c35ddf6503236526aa1711a7cb270ff9cd
Abhiram Nutakki <abhi@Abhirams-MacBook-Air.local>
2024-11-30 17:05:45 -0500

    first commit

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,15 +1,18 @@
 import time
-import streamlit as st
 import numpy as np
 from PIL import Image
 import urllib.request
 from utils import *
+import streamlit as st
+
+# Initialize image as None
+image = None  
 
 labels = gen_labels()
 
 html_temp = '''
     <div style =  padding-bottom: 20px; padding-top: 20px; padding-left: 5px; padding-right: 5px">
-    <center><h1>Garbage Segregation</h1></center>
+    <center><h1>Waste Wise</h1></center>
     
     </div>
     '''
@@ -21,12 +24,10 @@ html_temp = '''
     <center><h3>Please upload Waste Image to find its Category</h3></center>
     </div>
     '''
-st.set_option('deprecation.showfileUploaderEncoding', False)
 st.markdown(html_temp, unsafe_allow_html=True)
 opt = st.selectbox("How do you want to upload the image for classification?\n", ('Please Select', 'Upload image via link', 'Upload image from device'))
 if opt == 'Upload image from device':
     file = st.file_uploader('Select', type = ['jpg', 'png', 'jpeg'])
-    st.set_option('deprecation.showfileUploaderEncoding', False)
     if file is not None:
         image = Image.open(file)
 
